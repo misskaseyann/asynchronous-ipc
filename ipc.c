5ce3657dbf4e8dd005a0ff5af9a66a1d9f9e5d69
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-09-16 15:27:14 -0400

    added handlers.

--- ipc.c
+++ ipc.c
@@ -1,6 +1,10 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
+#include <signal.h>
+
+void sigIntHandler(int);
+void sigUsrHandler(int);
 
 /* 
  * Communicating Processes
@@ -14,7 +18,9 @@
  */
 int main() {
 	pid_t pid;
+
 	// TODO: install signal handler(s) for the user-defined signals (SIGUSR1/SIGUSR2)
+	signal(SIGINT, sigIntHandler);
 
 	// spawn off a child process
 	if ((pid = fork()) < 0) {
@@ -23,18 +29,31 @@ int main() {
 	}
 	// child process
 	else if (!pid) {
-		//while(1) {
+		while(1) {
 			int random = 3; // TODO make random
 			sleep(random);
 			printf("heyyy");
 			// TODO: randomly send one of the two user-defined signals to its parent
-		//}
-		exit(0); // temp to prevent background process
+		}
 	}
 	printf("spawned child PID# %d\n", pid);
+	pause();
 	// TODO: when a user-defined signal is raised, it reports the type of signal sent
 	// note: may be necessary to reinstall your signal handler after a signal is received
 	
-	// TODO: terminates gracefully upon receiving a control-c
-	exit(0); // temp
+	return 0; // temp
+}
+
+// TODO: terminates gracefully upon receiving a control-c
+void sigIntHandler(int signNum) {
+	printf("Control-c registered. Exiting...");
+	sleep(1);
+	printf("Bye!");
+	exit(0);
+}
+// TODO: finish handler for siguser calls.
+void sigUsrHandler(int signNum) {
+	printf("User signal registered....");
+	sleep(1);
+	printf("Keep doing your thing...");
 }
