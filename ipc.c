e4eeb87e0f6b82503d59b68ef613a8329e6e7362
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-09-17 16:43:55 -0400

    changed to look closer to the example

--- ipc.c
+++ ipc.c
@@ -24,58 +24,58 @@ int main() {
 	// Install signal handler(s) for the user-defined signals (SIGUSR1/SIGUSR2).
 	signal(SIGINT, sigUsrHandler);
 	signal(SIGUSR1, sigUsrHandler);
-	signal(SIGUSR2, sigUsrHandler); //only need one handler for all
+	signal(SIGUSR2, sigUsrHandler);
 
-	//vector holding the signals for random choosing
+	// Vector holding the signals for random choosing.
 	int signals[2] = {SIGUSR1, SIGUSR2};
 
-	//intro cat logo
+	// Intro cat logo.
 	printf("\n  /\\___/\\\n ( o   o )\n (  =^=  ) \n (        )\n (         )\n (          )))))))))))\n\n");
-	printf("~* Welcome to tinyshell by kehlsey & kasey!\n");
-	printf("~* To quit please use ctrl + c or forever be stuck looping in tinyshell. \n\n");
+	printf("~* Welcome to our experiment with asynchronous IPC by kehlsey & kasey!\n");
+	printf("~* To quit please use ctrl + c or forever be stuck looping. \n\n");
 
-	// spawn off a child process
+	// Spawn off a child process.
 	if ((pid = fork()) < 0) {
 		perror("fork failed");
 		exit(1);
 	}
 
-	// child process
+	// Child process.
 	else if (!pid) {
 
-		ppid = getppid(); //gettting parent id
+		ppid = getppid(); // gettting parent id
 
 		while(1) {
-			int random = rand() % (5 + 1 - 1) + 1; //generating a random number to sleep
-		
-			sleep(random);
-			printf("\ntinyshell slept for %d seconds...\n", random);	
-
+			int random = rand() % (5 + 1 - 1) + 1; // generating a random number to sleep
+			sleep(random);	
 			int chosen = signals[rand()%2];
-			kill(ppid,chosen); //sends SIGUSR1 or SIGUSR2 signal to parent 
+			kill(ppid,chosen); // sends SIGUSR1 or SIGUSR2 signal to parent 
 		}
-	}
 
-	printf("tinyshell spawned child PID# %d\n", pid);
-	waitpid(-1, &status, 0); //waiting for child process
-	
-	return 0; // temp
+	} 
+
+	printf("parent spawned child PID# %d\nwaiting... ", pid);
+	fflush(stdout);
+	waitpid(-1, &status, 0); // waiting for child process
+
+	return 0;
 }
 
+/* Handles SIGINT, SIGUSR1, and SIGUSR2 signals. */
 void sigUsrHandler(int signNum) { 
 
-	//terminates when control c recieved
-	if (signNum == SIGINT){
-		printf("Control-c registered. Exiting...\n");
+	// Terminates when control c recieved.
+	if (signNum == SIGINT) {
+		fflush(stdout);
+		printf(" received. That's it, I'm shutting you down...\n");
 		exit(0);
 	}
-
+	// Handle SIGUSR1 + SIGUSR2.
 	else if (signNum == SIGUSR1) {
-		printf("Recieved a SIGUSR1 signal\n");
+		printf("Recieved a SIGUSR1 signal\nwaiting... ");
 	}
-
 	else {
-		printf("Recieved a SIGUSR2 signal\n");
+		printf("Recieved a SIGUSR2 signal\nwaiting... ");
 	}
-
+	fflush(stdout);
 }
